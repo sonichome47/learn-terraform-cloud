077fa20697ee213eab68a514768e377f528aa503
sonichome47 <104440164+sonichome47@users.noreply.github.com>
2023-08-08 14:42:22 +0800

    Create newec2.tf

---
+++ shanghaiteam/newec2.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "ubuntu03" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "ec03"
+  }
+}
+resource "aws_instance" "ubuntu04" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "ec04"
+  }
+}
